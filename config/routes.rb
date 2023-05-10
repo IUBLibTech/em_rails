682e4b42ca0193d39d06e3fff8dc4a21f02f1713
jaalbrec <jaalbrec@indiana.edu>
2023-05-10 14:35:19 -0400

    Fixed a display bug with author names

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
 
-  # our PURL resolver has redirects to the old application resource pattern. For example:
-  # http://purl.dlib.indiana.edu/iudl/em/Alajaji/910339 redirects to
-  # https://ethnomultimedia.org/https://ethnomultimedia.org/media.html?aid=Alajaji/910339 which is the annotation with
-  # id 910339. The "aid" portion is a combination of author last name and annotation ID
-  get '/media.html', to: 'annotations#purl', as: "purl_redirector"
+  root "application#home"
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
@@ -16,15 +12,12 @@ Rails.application.routes.draw do
   get 'users/reset/:key', to: "users#reset", as: 'users_reset'
   post 'users/reset/:id', to: "users#reset"
 
-
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/signout' => 'sessions#destroy'
 
   get '/request_reset' => "users#reset_password"
   post '/request_reset' => "users#reset_password"
-
-
 
   get "books", to: "books#index", as: "books"
   get "book/:id", to: "books#show", as: "book"
@@ -36,7 +29,12 @@ Rails.application.routes.draw do
   get "faq", to: "application#faq", as: 'faq'
   get "contact", to: "application#contact", as: "contact"
 
-  root "application#home"
+
+  # our PURL resolver has redirects to the old application resource pattern. For example:
+  # http://purl.dlib.indiana.edu/iudl/em/Alajaji/910339 redirects to
+  # https://ethnomultimedia.org/https://ethnomultimedia.org/media.html?aid=Alajaji/910339 which is the annotation with
+  # id 910339. The "aid" portion is a combination of author last name and annotation ID
+  get '/media.html', to: 'annotations#purl', as: "purl_redirector"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
